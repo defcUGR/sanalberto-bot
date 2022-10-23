e12ee1fb7380da520a62c879cc8476817886fe90
Hipy Cas <hipycas@gmail.com>
2022-10-23 18:03:10 +0200

    Reorganiza module imports

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 
 import { SanAlbertoBot } from "./bot";
 
-import adminsModule from "./modules/admins";
 import baseModule from "./modules/base";
+import adminsModule from "./modules/admins";
 import dbModule from "./modules/db";
-import displayModule from "./modules/display";
 import pointsModule from "./modules/points";
+import displayModule from "./modules/display";
 
 dotenv.config();
 
